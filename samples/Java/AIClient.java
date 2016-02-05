d4e5ee65f969769d161c488dd558577f9cb9b700
Liam Coleman-Aulenbach <liamcoau@gmail.com>
2016-02-05 16:03:15 -0500

    Added basic scoring method for block placement.

--- samples/Java/AIClient.java
+++ samples/Java/AIClient.java
@@ -20,4 +20,36 @@ public class AIClient
             out.println("left");
         }
     }
+
+    public static double score(Board board) {
+        Point[] squares = board._block.squares();
+        int num_blocks = squares.length;
+
+        int heightScores = 0;
+
+        for (int i = 0; i < squares.length; i++) {
+            heightScores += squares[i].i;
+        }
+
+        int rowsCompleted = 0;
+        int clearScore = 0;
+
+        int[][] newBitmap = new int[board.ROWS][board.COLS];
+        for (int i = 0; i < board.ROWS; i ++) {
+            for (int j = 0; j < board.COLS; j ++) {
+                newBitmap[i][j] = board._bitmap[i][j];
+            }
+        }
+        for (int i = 0; i< board.ROWS; i++) {
+            if (board.isRowComplete(newBitmap, i)) {
+                rowsCompleted++;
+            }
+        }
+        clearScore = rowsCompleted*rowsCompleted;
+
+        double heightScoreFactor = 4.0;
+        double rowsCompletedFactor = 10.0;
+
+        return heightScoreFactor + rowsCompletedFactor;
+    }
 }
